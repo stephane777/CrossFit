2a680423cd9f0a5c7e60e4b1ce076f36494c7f22
Stephane Candelas <stephanecandelas@gmail.com>
2024-07-13 23:35:20 +0100

    feat: add v1 folder

--- crossfit-wod-api/src/index.js
+++ crossfit-wod-api/src/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req, res) =>{
-    res.send('<h3>Test</h3>')
-})
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () =>{
     console.log('API is listening to port: ', PORT)
